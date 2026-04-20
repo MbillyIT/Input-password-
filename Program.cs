0c9e23446ceeab70bccd0f9c09388c1bcb2c7deb
Max <maxbillyeven2@gmail.com>
2026-04-20 19:28:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.InputEncoding = System.Text.Encoding.UTF8;
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.WriteLine("введіть пароль:");
+        string input = Console.ReadLine();
+
+        if (input == "admin")
+        {
+            Console.WriteLine("Пароль правильний:");
+        }
+        else Console.WriteLine("Пароль не є правильним");
+    }
+}
